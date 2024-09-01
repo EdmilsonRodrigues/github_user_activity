a019946638a1be73e4891aae58aa7a3695ba207b
Edmilson Monteiro Rodrigues Neto <e.rodrigues@roborian.com>
2024-09-01 09:39:12 +0100

    Finished gitub_user_activity project

---
+++ github_activity.py
@@ -0,0 +1,65 @@
+import asyncio
+import aiohttp
+from sys import argv
+
+
+url = "https://api.github.com/users/{username}/events"
+EVENTS = ["issue", "commits"]
+
+
+def get_repo(activity: dict[str, dict[str, str]]) -> str:
+    return activity["repo"]["url"].split("/repos/")[-1]
+
+
+def categorize(categorized_activities: dict, payload: dict, repo: str) -> dict:
+    categorized_activities["others"] = []
+    added = False
+    for event in EVENTS:
+        if event in payload:
+            if event not in categorized_activities:
+                categorized_activities[event] = {}
+            if repo in categorized_activities[event]:
+                categorized_activities[event][repo] += 1
+                added = True
+            else:
+                categorized_activities[event][repo] = 1
+                added = True
+    else:
+        if not added:
+            categorized_activities["others"].append(payload)
+    return categorized_activities
+
+
+def get_categorized_activities(activities: list[dict[str, dict[str, str]]]):
+    categorized_activities = dict()
+    for activity in activities:
+        payload = activity["payload"]
+        repo = get_repo(activity)
+        categorized_activities = categorize(
+            categorized_activities=categorized_activities, payload=payload, repo=repo
+        )
+    return categorized_activities
+
+
+def print_categorized_activities(categorized_activites: dict[str, dict | list]):
+    print("Output:")
+    for commit in categorized_activites["commits"]:
+        print(f"- Pushed {categorized_activites["commits"][commit]} commits to {commit}")
+    for issue in categorized_activites["issue"]:
+        print(f"- Opened {categorized_activites["issue"][issue]} new issue in {issue}")
+
+
+async def get_activity():
+    if len(argv) != 2:
+        raise SystemError("Usage: python github_activity <username>")
+    username = argv[1]
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url.format(username=username)) as response:
+            activities = await response.json()
+    categorized_activities = get_categorized_activities(activities=activities)
+    return categorized_activities
+
+
+if __name__ == "__main__":
+    categorized_activites = asyncio.run(get_activity())
+    print_categorized_activities(categorized_activites=categorized_activites)
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,39 @@
 import pytest
-from main import *
+import github_activity
+
+
+@pytest.fixture
+def username():
+    return "EdmilsonRodrigues"
 
 
 @pytest.mark.asyncio(loop_scope="session")
-python 
+async def test_fail_get_github_activity():
+    github_activity.argv = ["github_activity"]
+    with pytest.raises(SystemError):
+        assert await github_activity.get_activity() is not None
+
+
+@pytest.mark.asyncio(loop_scope="session")
+async def test_fail_get_github_activity_2():
+    github_activity.argv = ["github_activity", "username", "wrong"]
+    with pytest.raises(SystemError):
+        assert await github_activity.get_activity() is not None
+
+
+@pytest.mark.asyncio(loop_scope="session")
+async def test_get_github_activity(username):
+    github_activity.argv = ["github_activity", username]
+    assert await github_activity.get_activity() is not None
+    assert list((await github_activity.get_activity()).keys()) == ["others", "commits", "issue"]
+
+
+"""
+  +     'id',
+  +     'type',
+  +     'actor',
+  +     'repo',
+  +     'payload',
+  +     'public',
+  +     'created_at',
+"""
